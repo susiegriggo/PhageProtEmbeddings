e8eb5368bc81dce62aabd4720cad09a9ca31a919
susiegriggo <susie.grigson@gmail.com>
2024-01-22 10:55:54 +1030

    fix printouts

--- run_clustering.py
+++ run_clustering.py
@@ -1,7 +1,7 @@
 """
 Run clustering
 
-Currently saves the best labels based on the silhouette score. May change to use whichever score is best
+Currently saves the best labels based on the inertia as silhouette score and calinski harabasz index are too slow
 """
 
 __author__ = "Susanna Grigson"
@@ -15,7 +15,7 @@ __status__ = "development"
 # imports
 import pandas as pd
 import click
-from sklearn.metrics import silhouette_score, calinski_harabasz_score
+#from sklearn.metrics import silhouette_score, calinski_harabasz_score
 import numpy as np
 from dask_ml.cluster import KMeans
 import dask.array as da
@@ -69,77 +69,57 @@ import time
 def main(n_samples, k_clusters, bootstraps, data, batch_size, out):
 
     # read in the data
-    print('reading in the embeddings')
+    print('Reading in protein embeddings located at :' + str(data))
     embeddings = pickle.load(open(data, 'rb'))
-    #print(list(embeddings.values())[:4]) 
 
     # array to store data
-    print('preparing arrays')
     inertia = np.zeros(bootstraps)
-    #silhouette = np.zeros(bootstraps)
-    #ch = np.zeros(bootstraps)
-
 
     # store the best silhouette score and labels
     best_score = np.inf
     best_labels = None
 
     # loop through the bootstraps
-    print('Running bootstraps')
+    print(str(bootstraps) + 'have been specified. Looping through bootstraps...')
     for b in range(bootstraps):
 
         # print an update
-        print(b, flush = True)
+        print( '\t bootstrap: ' + str(b), flush = True)
 
         # get a subsample of the data
-        print('generate subsample index', flush = True )
-        #idx = np.random.randint(0, len(embeddings), n_samples)
-        #embedding_subset = np.array(list(embeddings.values()))[idx].astype(np.float32)
-        #embedding_subset = [list(embeddings.values())[i] for i in idx]
-        #embedding_subset = np.array(embedding_subset).astype(np.float32)
+        print('\t generating subsample of size ' + str(n_samples), flush = True )
         idx = np.random.choice(list(embeddings.keys()), n_samples, replace=True)
-        print('generate subsample', flush = True )
         embedding_subset = np.array([embeddings[i] for i in idx], dtype=np.float32)
 
         # turn array into a dask array
-        print('make dask array', flush = True)
         embedding_dask = da.from_array(embedding_subset, chunks=(batch_size, 1280))
 
         # run through the clustering
-        print('run kmeans', flush = True)
-        print(k_clusters)
+        print('\t running kmeans for k=' + str(k_clusters), flush = True)
         start = time.time() 
         kmeans = KMeans(n_clusters=k_clusters, random_state=42) #TODO see if there are other parameters that should be included here
         kmeans.fit(embedding_dask)
         kmeans_labels = kmeans.labels_
         end = time.time() 
-        print(end - start)
+        print('\t clustering completed in ' + str(end - start) + ' seconds')
 
         # get silhouette and ch score score
-        print('compute scores', flush = True)
-        #start = time.time()
+        print('\t computing clustering scores', flush = True)
         #s_score = silhouette_score(embedding_subset, kmeans_labels, metric = 'cosine')
         #ch_score = calinski_harabasz_score(embedding_subset, kmeans_labels, metri = 'cosine')
 
         # store the scores
         inertia[b] = kmeans.inertia_
-        #silhouette[b] = s_score
-        #ch[b] = ch_score
-        #end = time.time() 
-        #print(end - start)
 
         # if the clustering is better update the saved labels
-        print('update labels', flush = True)
+        print('\t updating labels', flush = True)
         if kmeans.inertia_ < best_score:
 
-            #best_labels = dict(zip([list(embeddings.keys())[i] for i in idx], list(np.asarray(kmeans_labels)))) 
             best_labels = dict(zip(idx, list(np.asarray(kmeans_labels))))
-            #best_labels = best_labels = dict(zip(np.random.choice(list(embeddings.keys()), n_samples, replace=False), kmeans_labels))
             best_score = kmeans.inertia_
 
     # form a dataframe for these metrics and save it
-    print('save scores to file', flush = True)
-    #scores = pd.DataFrame({"inertia": inertia, "silhouette": silhouette, "calinski_harabasz": ch})
+    print('\n DONE\n saving output to ' + out, flush = True)
     scores = pd.DataFrame({"inertia": inertia}) 
 
     # save the dataframe
@@ -147,7 +127,6 @@ def main(n_samples, k_clusters, bootstraps, data, batch_size, out):
 
     # save the best labels
     pickle.dump(best_labels, open(out + '_bestlabels.pkl', 'wb'))
-
 
 if __name__ == "__main__":
     main()
